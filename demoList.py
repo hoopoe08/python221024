7bf720218c899386a7b1b896e1aaf8a64ae3da09
hoopoe <hoopoe08@gmail.com>
2022-10-24 15:12:42 +0900

    first commit

---
+++ demoList.py
@@ -0,0 +1,19 @@
+# demoList.py
+
+strA = "python is very powerful"
+
+#문자열의 슬라이싱과 인덱싱
+print(strA[0:6])
+print(strA[:6])
+print(strA[-3:])
+
+lst = [10, 20, 30, 40, 50]
+print(lst[:2])
+print(lst[-2:])
+
+#debug 환경 설정
+#중단점을 지정(디버깅시에 중단) : 빨간 점
+for i in lst:
+    print(i)
+
+    
